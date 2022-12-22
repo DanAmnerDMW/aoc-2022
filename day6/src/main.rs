05f461600077da8730da1b93d3bde5e3aeb106c8
Daniel Amner <daniel.amner@credera.co.uk>
2022-12-22 15:18:08 +0000

    Day 6

---
+++ day6/src/main.rs
@@ -0,0 +1,44 @@
+use std::fs;
+
+const INPUT_SIZE: usize = 14;
+
+fn read_input_file() -> String {
+    return fs::read_to_string("input.txt").expect("Unable to read input file");
+}
+
+fn is_marker(input: &str) -> bool {
+    for c in input.chars() {
+        let mut char_count = 0;
+        for test_char in input.chars() {
+            if c.eq(&test_char) {
+                char_count += 1
+            }
+            if char_count == 2 {
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
+
+fn main() {
+    let file_input = read_input_file();
+
+    let first_chars = &file_input[0..INPUT_SIZE];
+
+    let mut marker = 0;
+
+    if is_marker(&first_chars) {
+        marker = INPUT_SIZE;
+    }
+
+    for i in 1..=(file_input.len() - INPUT_SIZE) {
+        if is_marker(&file_input[i..i + INPUT_SIZE]) {
+            marker = i + INPUT_SIZE;
+            break;
+        }
+    }
+
+    println!("{}", marker);
+}
